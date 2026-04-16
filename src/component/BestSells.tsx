e55aff832937c4c6279af09dc36485f569558914
MadhuBala <smadhubala101@gmail.com>
2026-04-16 12:47:05 -0500

    mapped bestsells

--- src/component/BestSells.tsx
+++ src/component/BestSells.tsx
@@ -4,15 +4,144 @@ import crushedTomatoes from '../assets/bestsells/sell-3.jpg';
 import pineapple from '../assets/bestsells/sell-4.jpg';
 import './css/PopularProducts.css';
 
+const data = [
+  {
+    imgUrl: coffeeBeans,
+    title: "100% Organic Coffee Beans.",
+    subTitle: "Get the best deal before close.",
+    button: " Shop Now",
+  },
+  {
+    imgUrl: groundcoffee,
+    title: "Roast Ground Coffee",
+    text: "Tea, Coffee & Drinks",
+    dallar: 13,
+    dallarNumber: 18,
+    star: 4.5,
+    button: "Add to cart",
+    days: "Days",
+    daysNumber: 907,
+    hours: "Hours",
+    hoursNumber: 11,
+    min: "Mins",
+    minsNumber: 57,
+    sec: "Sec",
+    secNumber: 24,
+  },
+  {
+    imgUrl: crushedTomatoes,
+    title: "Crushed Tomatoes",
+    text: "Fruits & Vegetables",
+    dallar: 13,
+    dallarNumber: 18,
+    star: 4.5,
+    button: "Add to cart",
+    days: "Days",
+    daysNumber: 967,
+    hours: "Hours",
+    hoursNumber: 11,
+    min: "Mins",
+    minsNumber: 52,
+    sec: "Sec",
+    secNumber: 24,
 
+  },
+  {
+    imgUrl: pineapple,
+    title: "Golden Pineapple",
+    text: "Fruits & Vegetables",
+    dallar: 13,
+    dallarNumber: 18,
+    star: 4.5,
+    button: "Add to cart",
+    days: "Days",
+    daysNumber: 939,
+    hours: "Hours",
+    hoursNumber: 11,
+    min: "Mins",
+    minsNumber: 52,
+    sec: "Sec",
+    secNumber: 24,
+
+  }
+
+]
 const BestSells = () => {
   return (
     <>
       <div className='best-sell'>
         <h3 className='mt-5 '>Daily Best Sells</h3>
         <div className="row row-cols-1 row-cols-md-4 g-4 mt-2">
+          {data.map((item, index) => (
+            <div className="col" key={index}>
+              {index === 0 ? (
+                <div className="card custom-card h-100 w-100">
+                  <img src={item.imgUrl} className="card-img-top" alt="coffeeBeans" />
+                  <div className="card-img-overlay ms-4 text-white d-flex flex-column ">
+                    <h6 className="card-title fs-4  fw-bolder">{item.title}</h6>
+                    <p className="card-text ">{item.subTitle}</p>
+                    <div className='w-100 d-block m-0'>
+                      <button className=' rounded-2 p-2 px-3   border-0 fw-900 text-white bgclor'> {item.button}<i className="bi bi-arrow-right-short"></i></button>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="card h-100 rounded-3">
+                  <img src={item.imgUrl} className="card-img-top m-0 " alt="groundcoffee" />
+                  <div className="card-body  m-0">
+                    <p className="card-text text-muted m-1 fontChange" >{item.text}</p>
+                    <p className="card-title text-bold fw-bolder fontChange body m-1 ">{item.title}</p>
+                    <div className='d-flex justify-content-between '>
+                      <div>
+                        <span className='fw-bolder fontChange'><i className="bi bi-currency-dollar"></i>{item.dallar}</span>
+                        <span className='text-muted m-0 fontChange'><i className="bi bi-currency-dollar "></i>{item.dallarNumber}</span>
+                      </div>
+                      <div>
+                        <p className='mt-2 fontChange'><i className="bi bi-star-fill text-warning "></i> <i className="bi bi-star-fill text-warning"></i> <i className="bi bi-star-fill text-warning"></i> <i className="bi bi-star-fill text-warning"></i> <i className="bi bi-star-fill text-warning"></i> <span className='text-muted'>{item.star}</span> </p>
+                      </div>
+                    </div>
+                    <div>
+                      <button className='addbtn p-2 rounded-3 w-100 text-white fw-bolder'> <i className="bi bi-plus"></i>{item.button}</button>
+                    </div>
+                    <div className="row mt-3  justify-content-center text-center ">
+                      <div className="col-3   px-1">
+                        <div className='card mx-0 p-2 d-flex flex-column align-item-center justify-content-center'>
+                          <span className='fntsze fw-bolder'>{item.daysNumber}</span>
+                          <span className='fntsze text-muted'>{item.days}</span>
+                        </div>
+
+                      </div>
+                      <div className="col-3  px-1 ">
+                        <div className='card p-2 d-flex flex-column align-item-center justify-content-center'>
+                          <span className='fntsze fw-bolder'>{item.hoursNumber}</span>
+                          <span className='fntsze text-muted'>{item.hours}</span>
+                        </div>
+                      </div>
+                      <div className="col-3 px-1">
+                        <div className='card p-2 d-flex flex-column align-item-center justify-content-center'>
+                          <span className='fntsze fw-bolder'>{item.minsNumber}</span>
+                          <span className='fntsze text-muted'>{item.min}</span>
+                        </div>
+                      </div>
+
+                    </div>
+                    <div className="row   justify-content-center text-center">
+                      <div className="col-3 mt-2 px-1">
+                        <div className='card p-2  text-center d-flex flex-column align-item-center justify-content-center'>
+                          <span className='fntsze fw-bolder'>{item.secNumber}</span>
+                          <span className='fntsze text-muted'>{item.sec}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+            </div>
+          ))}
+
           {/* col-1 */}
-          <div className="col ">
+          {/* <div className="col ">
             <div className="card custom-card h-100 w-100">
               <img src={coffeeBeans} className="card-img-top" alt="coffeeBeans" />
               <div className="card-img-overlay ms-4 text-white d-flex flex-column ">
@@ -23,10 +152,10 @@ const BestSells = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
           {/* col-2 */}
 
-          <div className="col">
+          {/* <div className="col">
             <div className="card h-100 rounded-3">
               <img src={groundcoffee} className="card-img-top m-0 " alt="groundcoffee" />
               <div className="card-body  m-0">
@@ -76,10 +205,10 @@ const BestSells = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
           {/* col-3 */}
 
-          <div className="col">
+          {/* <div className="col">
             <div className="card h-100 rounded-3">
               <img src={crushedTomatoes} className="card-img-top m-0 " alt="crushedTomatoes" />
               <div className="card-body  m-0">
@@ -129,10 +258,10 @@ const BestSells = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
           {/* col-4*/}
 
-          <div className="col">
+          {/* <div className="col">
             <div className="card h-100 rounded-3">
               <img src={pineapple} className="card-img-top m-0 " alt="pineapple" />
               <div className="card-body  m-0">
@@ -182,7 +311,7 @@ const BestSells = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
 
       </div >
@@ -191,9 +320,3 @@ const BestSells = () => {
 }
 
 export default BestSells
-
-
-{/* <figure class="figure">
-  <img src="..." class="figure-img img-fluid rounded" alt="...">
-  <figcaption class="figure-caption">A caption for the above image.</figcaption>
-</figure> */}
--- src/component/FeatureCarousal.tsx
+++ src/component/FeatureCarousal.tsx
@@ -8,9 +8,14 @@ import chicken from "../assets/featured/2/category-chicken-meat-fish.jpg";
 import teaCoffee from "../assets/featured/2/category-tea-coffee-drinks.jpg";
 import surfExcel from "../assets/featured/3/category-cleaning-essentials.jpg";
 import pet from "../assets/featured/3/category-pet-care.jpg";
+import Carousal from './Carousal';
 
 
-const datas = [{
+interface Carousal {
+  imagUrl: string;
+  title: string;
+}
+const datas: Carousal[] = [{
   imagUrl: dairyBread,
   title: 'Dairy, Bread & Eggs'
 },
@@ -52,91 +57,56 @@ const datas = [{
 }
 
 ];
+const dataArr = (arr: Carousal[], size: number) => {
+  const result: Carousal[][] = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+
+const groupData = dataArr(datas, 4);
 const FeatureCarousal = () => {
   return (
     <>
-      {/* <div className="row row-cols-1 row-cols-md-5 g-4 mt-1">
-        {/* col-1-sev */}
-      {/* {datas.map((item, index) => (
-          <div key={index} className="col">
-            <div className="card  rounded-3">
-              <div className='w-100 d-block ' >
-                <span className=' badge bg-danger stale '>{item.text}</span>
-              </div>
-              <img src={item.imgUrl} className="card-img-top m-0 imgSize " alt="sev" />
-              <div className="card-body  m-0">
-                <p className="card-text text-muted m-0 fontChange" >{item.subTitle}</p>
-                <p className="card-title text-bold fw-bolder fontChange body m-0 ">{item.title}</p>
-                <p className='mt-2 fontChange'><i className="bi bi-star-fill text-warning "></i> <i className="bi bi-star-fill text-warning"></i> <i className="bi bi-star-fill text-warning"></i> <i className="bi bi-star-fill text-warning"></i> <i className="bi bi-star-fill text-warning"></i> <span className='text-muted'>{item.starValue}</span> </p>
-                <div className='d-flex justify-content-between '>
-                  <div>
-                    <span className='fw-bolder fontChange'><i className="bi bi-currency-dollar"></i>{item.dallarValue}</span>
-                    {item.dallarNumber ? <span className='text-muted m-0 fontChange'><i className="bi bi-currency-dollar "></i>{item.dallarNumber}</span> : ""}
-                  </div>
-                  <div>
-                    <button className='addbtn m-0 text-white fw-800'> <i className="bi bi-plus"></i> {item.button}</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))} */}
 
-      {/* </div> */}
       <div className='mt-5'>
         <h4>Featured Categories</h4>
-        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleControls" className="carousel slide position-relative" data-bs-ride="carousel">
           <div className="carousel-inner">
-            <div className="row row-cols-1 row-cols-md-4 g-2 mt-1">
-              <div className="col">
-                {datas.map((items, index) => (
+            {groupData.map((group, i) => (
 
-                  <div key={index} className={`carousel-item ${index === 0 ? "active" : ""} `}>
-                    <div className="card">
-                      <figure className="figure text-center ">
-                        <img src={items.imagUrl} className="figure-img img-fluid rounded" alt="dairyBread" />
-                        <figcaption className="figure-caption">{items.title}</figcaption>
-                      </figure>
+              <div key={i} className={`carousel-item ${i === 0 ? "active" : ""} `}>
+                <div className='row row-cols-1 row-cols-md-4  '>
+                  {group.map((item, index) => (
+                    <div key={index} className="col">
+                      <div className="card">
+                        <figure className="figure text-center ">
+                          <img src={item.imagUrl} className="figure-img img-fluid rounded" alt={item.title} />
+                          <figcaption className="figure-caption">{item.title}</figcaption>
+                        </figure>
+                      </div>
+
                     </div>
-                  </div>
-
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
 
-            {/* // <div key={index} className={`carousel-item ${index === 0 ? "active" : ""} `}>
-              //   <div className="row row-cols-4">
-              //     <div className="card"><figure className="figure text-center ">
-              //       <img src={item.imagUrl} className="figure-img img-fluid rounded" alt="dairyBread" />
-              //       <figcaption className="figure-caption">{item.title}</figcaption>
-              //     </figure></div>
-              //     <div className="card"><figure className="figure text-center ">
-              //       <img src={item.imagUrl} className="figure-img img-fluid rounded" alt="dairyBread" />
-              //       <figcaption className="figure-caption">{item.title}</figcaption>
-              //     </figure></div>
-              //     <div className="card"><figure className="figure text-center">
-              //       <img src={item.imagUrl} className="figure-img img-fluid rounded" alt="dairyBread" />
-              //       <figcaption className="figure-caption">{item.title}</figcaption>
-              //     </figure></div>
-              //     <div className="card"><figure className="figure text-center ">
-              //       <img src={item.imagUrl} className="figure-img img-fluid rounded" alt="dairyBread" />
-              //       <figcaption className="figure-caption">{item.title}</figcaption>
-              //     </figure></div>
-              //   </div>
-              // </div>
-           */}
+            ))}
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
         </div>
-      </div >
 
+      </div>
+      <div className='position-relative start-0 end-0 d-flex gap-2 mt-2 me-2'>
+        <button className="carousel-control-prev  bg-info" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next bg-info " type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span className="carousel-control-next-icon  " aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+</div>
     </>
   )
 }
